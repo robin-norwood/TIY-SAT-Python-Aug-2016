8a7e595c2ff07c4d2ccbe8a87f06371747f07622
Robin Norwood <robin@theironyard.com>
2016-08-08 09:45:31 -0500

    Initial commit for week 2 day 1 code.

---
+++ week2/01.interactive.strings_and_floats.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+17
+12
+8
+3.141459
+8.7
+3 + 5
+2 / 7
+8 - 3
+2 * 4
+5 // 2
+5 % 2
+5 ** 2
+2 ** 10
+3 ** 3
+8.1 + 3
+float(8)
+int(0.25)
+int(3/4)
+4.0 ** 2
+3 - 5** 2 / 10 + 8 * 4
+3 - (( 5 ** 2) / 10) + (8 * 4)
+3  2 ** 5
+3 + 2 ** 5
+11 / 3
+( 6 / 3) + ( 5/3)
+( 6 / 3) + ( 5/3) == 11 /3
+.1 + .1 + .1
+(.1 + .1 + .1) * 3
+"Hello, world!"
+'Hello, world!'
+'Hello, world!"
+"I can't get over how much fun Python is!!"
+'I can\'t get over how much fun Python is!!'
+'I can\'t get over how much\n fun Python is!!'
+print('I can\'t get over how much\n fun Python is!!')
+print('I can\'t get over how much\n fun\t\t Python is!!')
+print("I want this string
+print('''This string
+is strung
+over several
+lines.'
+asdfa
+''')
+5 + 3
+"The plus " + 'operator concatenates ' + """strings"""
+"Spam " * 4
+'abcdefghijk'
+'abcdefghijk'[0]
+'abcdefghijk'[5]
+'abcdefghijk'[-1]
+'abcdefghijk'[-5]
+'abcdefghijk'[5]
+'abcdefghijk'[15]
+'abcdefghijk'[0:5]
+'abcdefghijk'[2:5]
+"This is the song that never ends, it goes on and on and on..."
+"This is the song that never ends, it goes on and on and on..."[4:19]
+"This is the song that never ends, it goes on and on and on..."[19:]
+"This is the song that never ends, it goes on and on and on..."[0:19]
+"This is the song that never ends, it goes on and on and on..."[19:]
+"This is the song that never ends, it goes on and on and on..."[-6:]
+"This is the song that never ends, it goes on and on and on..."[-6:-1]
+"This is the song that never ends, it goes on and on and on..."[0:10:2]
+'Hey! Who moved my cheese?'[5:-1:3]
+'0H1a2v3e4 5y6o7u8 9h0a1d2 3y4o5u6r7 8e9y0e1 2e3x4a5m6?'[1::2]
+"One " + "two"
+"One " + 5
+str(5)
+str(2.14)
+int("55")
+int("55.05")
+float("55.05")
+float(5)
+float("1.38")
+"One " + 5
+"One " + str(5)
+get_ipython().magic('save 01.interactive.strings_and_floats 1-72')

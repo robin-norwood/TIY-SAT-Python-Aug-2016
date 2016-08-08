070b8acb1451e424c7f65a3ebe78c6a380c65517
Robin Norwood <robin@theironyard.com>
2016-08-08 10:25:50 -0500

    Add classwork for lists, tuples, and dicts.

---
+++ week2/01.interactive.lists_tuples_dicts.py
@@ -0,0 +1,139 @@
+# coding: utf-8
+count = 5
+print(count)
+count = 10
+print(count)
+count * 3.14
+10 * 3.14
+total = count * 3.5
+total
+count = 5
+total
+count = 4.14
+count = "Dracula"
+print(count)
+total = count * 3.5
+count
+a_number = 103
+a_number = float(a_number)
+a_number
+a_number = "nope"
+a_number
+float(a_number)
+3 + 5
+total = 0
+total += 6
+total
+total += 6
+total += 6
+total += 6
+total
+total = total + 6
+total
+total -= 1
+total
+total -= 1
+total
+total -= 1
+total
+total *= 4
+total
+total /= 2
+total
+total **= 3
+total
+int(total)
+greeting = "Hello"
+print(greeting + ", friend!")
+greeting = "Knoichi wa"
+print(greeting + ", friend!")
+username = "Joe"
+print(greeting ", " + username)
+print(greeting + ", " + username)
+greeting = "Hello"
+print(greeting + ", " + username)
+greeting = 5
+print(greeting + ", " + username)
+good_numbers = [1, 1, 2, 3, 5, 8, 13, 21]
+bad_numbers = [4, 6, 7, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20]
+print(good_numbers)
+print(bad_numbers)
+good_numbers[0]
+good_numbers[0:3]
+good_numbers[0:4]
+good_numbers[4]
+good_numbers[0:4]
+bad_numbers[0:10:2]
+bad_numbers[::2]
+bad_numbers[0::2]
+bad_numbers[::2]
+bad_numbers[::2]
+bad_numbers
+bad_numbers[1::2]
+[1.2, 2.3, 3.14]
+lyrics = ["Hello", "Goodbye", "I say high", 'you say low']
+lyrics
+lyrics[1:5]
+lyrics = ["Hello", "Goodbye", "I say high", 'you say low', 3, 8, 5.3]
+lyrics
+lyrics[2]
+lyrics[2][3]
+lyrics[2][3:]
+lyrics
+lyrics = lyrics[4]
+lyrics
+lyrics = ["Hello", "Goodbye", "I say high", 'you say low', 3, 8, 5.3]
+lyrics
+lyrics = lyrics[0:4]
+lyrics
+lyrics[-1] = "you say WAZZUP DUDE"
+lyrics
+lyrics[-1][5] = "f"
+a_list = [0, 1, 2, 3, 4]
+a_list * 2
+a_list + a_list
+a_list += a_list * 2
+a_list
+a_list += 5
+a_list
+a_list = a_list + [5]
+a_list
+a_list - 5
+a_list.remove(5)
+a_list
+a_list.remove(0)
+a_list
+del a_list[0]
+a_list
+a_list.pop()
+a_list.pop()
+a_list
+dwavers = ('Doc', 'Happy', 'Sleepy', 'Grumpy', 'Dopey', 'Bashful')
+dwarves = ('Doc', 'Happy', 'Sleepy', 'Grumpy', 'Dopey', 'Bashful')
+dwarves
+dwavers[0:2]
+dwavers[0] = 'Joe'
+dwarves[1] = 5
+dwarves = (1, 2, 3)
+years = { 1492: "Columbus sailed the ocean blue.", 1616: "Death of shakespeare", 1776: "Decleration of Independence"}
+print(years)
+years[1616]
+colors = {"red": 16, "blue": 24, "yellow": 36}
+colors
+colors['red']
+colors['red'] = 8
+colors
+colors["mint"]
+office_workers = {"Las Vegas": ["Jo", "Steve", "Other Jo"], "San Diego": ["Max"], "Raleigh": ["BR-549", "THX-1138", "Voltron"]}
+office_workers['Raleigh']
+office_workers['Raleigh'].pop()
+office_workers['Raleigh']
+office_workers["Las Vegas"] += ["Voltron"]
+office_workers
+strange = {1: "one", "two": (1, 2, 3), 3.1: "Joe"}
+strange
+strange[1]
+strange[3.1]
+strange['two']
+get_ipython().magic('save 01.interactive.lists_tuples_dicts')
+get_ipython().magic('save 01.interactive.lists_tuples_dicts 1-138')
